cad1140e4b818256b745a062fce743e33fa71591
andrealiu <andrealiu@berkeley.edu>
2017-11-03 15:37:40 -0700

    added events and working on popup

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -17,6 +17,7 @@
  * under the License.
  */
 var tabs = ["map-tab", "events-tab", "leaderboard"]
+var coordinates =[[-122.253281, 37.870222], [-122.259604, 37.872446], [-122.264737, 37.873422], [-122.262816, 37.868604]]
 var app = {
     // Application Constructor
     initialize: function() {
@@ -43,10 +44,17 @@ var app = {
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        // listeningElement.setAttribute('style', 'display:none;');
+        // receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    wait: function(ms){
+       var start = new Date().getTime();
+       var end = start;
+       while(end < start + ms) {
+         end = new Date().getTime();
+      }
     },
     showStartPage: function() {
         document.getElementById('startpage').style.display = 'block'; 
@@ -54,6 +62,8 @@ var app = {
         document.getElementById('homepage').style.display = 'none';
         app.initializeMap()
         document.body.style.backgroundImage = "none";
+        var popup = document.getElementById("instructions");
+        popup.classList.add("show");
     },
     initializeMap: function() {
         mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcmVhbGl1IiwiYSI6ImNqOWdudjNpdDJ6eXoyd3A5MmxxeTV1ZGMifQ.2P9d2RHzp_oDPb1IfasI4g';
@@ -71,6 +81,200 @@ var app = {
         }));
         console.log("hello");
         map.resize()
+        // for (var i = 1; i < 5; i++) {
+        //     var imageURL = "icon" + i.toString() + ".png"
+        //     var iconName = "icon" + i.toString()
+        //     var idName = "points" + i.toString()
+        //     console.log("image url", imageURL)
+        //     map.on('load', function() {
+        //         map.loadImage(imageURL, function(error, image) {
+        //             if (error) throw error;
+        //             map.addImage(iconName, image);
+        //             map.addLayer({
+        //                 "id": idName,
+        //                 "type": "symbol",
+        //                 "source": {
+        //                     "type": "geojson",
+        //                     "data": {
+        //                         "type": "FeatureCollection",
+        //                         "features": [{
+        //                             "type": "Feature",
+        //                             "properties": {
+        //                                 "description": "<p id='description'>Description 1.</p>",
+        //                                 "icon":  iconName
+        //                             },
+        //                             "geometry": {
+        //                                 "type": "Point",
+        //                                 "coordinates": coordinates[i-1]
+        //                             }
+        //                         }]
+        //                     }
+        //                 },
+        //                 "layout": {
+        //                     "icon-image": iconName,
+        //                     "icon-size": 0.25
+        //                 }
+        //             });
+        //         });
+        //     });
+        //     map.on('click', idName, function (e) {
+        //         new mapboxgl.Popup()
+        //             .setLngLat(e.features[0].geometry.coordinates)
+        //             .setHTML(e.features[0].properties.description)
+        //             .addTo(map);
+        //     });
+        // }
+        map.on('load', function() {
+            map.loadImage('icon1.png', function(error, image) {
+                if (error) throw error;
+                map.addImage('icon1', image);
+                map.addLayer({
+                    "id": "points",
+                    "type": "symbol",
+                    "source": {
+                        "type": "geojson",
+                        "data": {
+                            "type": "FeatureCollection",
+                            "features": [{
+                                "type": "Feature",
+                                "properties": {
+                                    "description": "<p id='description'>Description 1.</p>",
+                                    "icon":  "icon1"
+                                },
+                                "geometry": {
+                                    "type": "Point",
+                                    "coordinates": [-122.253281, 37.870222]
+                                }
+                            }]
+                        }
+                    },
+                    "layout": {
+                        "icon-image": "icon1",
+                        "icon-size": 0.25
+                    }
+                });
+            });
+        });
+        map.on('load', function() {
+            map.loadImage('icon2.png', function(error, image) {
+                if (error) throw error;
+                map.addImage('icon2', image);
+                map.addLayer({
+                    "id": "points2",
+                    "type": "symbol",
+                    "source": {
+                        "type": "geojson",
+                        "data": {
+                            "type": "FeatureCollection",
+                            "features": [{
+                                "type": "Feature",
+                                "properties": {
+                                    "description": "<p id='description'>Description 2.</p>",
+                                    "icon":  "icon2"
+                                },
+                                "geometry": {
+                                    "type": "Point",
+                                    "coordinates": [-122.259604, 37.872446]
+                                }
+                            }]
+                        }
+                    },
+                    "layout": {
+                        "icon-image": "icon2",
+                        "icon-size": 0.25
+                    }
+                });
+            });
+        });
+        map.on('load', function() {
+            map.loadImage('icon3.png', function(error, image) {
+                if (error) throw error;
+                map.addImage('icon3', image);
+                map.addLayer({
+                    "id": "points3",
+                    "type": "symbol",
+                    "source": {
+                        "type": "geojson",
+                        "data": {
+                            "type": "FeatureCollection",
+                            "features": [{
+                                "type": "Feature",
+                                "properties": {
+                                    "description": "<p id='description'>Description 3.</p>",
+                                    "icon":  "icon3"
+                                },
+                                "geometry": {
+                                    "type": "Point",
+                                    "coordinates": [-122.264737, 37.873422]
+                                }
+                            }]
+                        }
+                    },
+                    "layout": {
+                        "icon-image": "icon3",
+                        "icon-size": 0.25
+                    }
+                });
+            });
+        });
+        map.on('load', function() {
+            map.loadImage('icon4.png', function(error, image) {
+                if (error) throw error;
+                map.addImage('icon4', image);
+                map.addLayer({
+                    "id": "points4",
+                    "type": "symbol",
+                    "source": {
+                        "type": "geojson",
+                        "data": {
+                            "type": "FeatureCollection",
+                            "features": [{
+                                "type": "Feature",
+                                "properties": {
+                                    "description": "<p id='description'>Description 4.</p>",
+                                    "icon":  "icon4"
+                                },
+                                "geometry": {
+                                    "type": "Point",
+                                    "coordinates": [-122.262816, 37.868604]
+                                }
+                            }]
+                        }
+                    },
+                    "layout": {
+                        "icon-image": "icon4",
+                        "icon-size": 0.25
+                    }
+                });
+            });
+        });
+        map.on('click', 'points', function (e) {
+            new mapboxgl.Popup()
+                .setLngLat(e.features[0].geometry.coordinates)
+                .setHTML(e.features[0].properties.description)
+                .addTo(map);
+        });
+        map.on('click', 'points2', function (e) {
+            new mapboxgl.Popup()
+                .setLngLat(e.features[0].geometry.coordinates)
+                .setHTML(e.features[0].properties.description)
+                .addTo(map);
+        });
+        map.on('click', 'points3', function (e) {
+            new mapboxgl.Popup()
+                .setLngLat(e.features[0].geometry.coordinates)
+                .setHTML(e.features[0].properties.description)
+                .addTo(map);
+        });
+        map.on('click', 'points4', function (e) {
+            new mapboxgl.Popup()
+                .setLngLat(e.features[0].geometry.coordinates)
+                .setHTML(e.features[0].properties.description)
+                .addTo(map);
+        });
+        map.on('click', 'points1', function (e) {
+            map.flyTo({center: e.features[0].geometry.coordinates});
+        });
     },
     changeActiveTab: function(id) {
         document.getElementById(id).classList.add("active")
